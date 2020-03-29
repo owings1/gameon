4f497c00c484bc4eeb2431685ffe4c60cad9897e
Doug Owings <doug@dougowings.net>
2020-03-28 19:46:30 -0700

    move undo, state string, tests

--- src/lib/game.js
+++ src/lib/game.js
@@ -5,8 +5,12 @@ const merge  = require('merge')
 const {intRange} = Util
 const sp         = Util.joinSpace
 
-const Red   = 'Red'
 const White = 'White'
+const Red   = 'Red'
+const ColorAbbr = {
+    White : 'W'
+  , Red   : 'R'
+}
 
 const InsideSlots = {
     White : intRange(18, 23)
@@ -28,12 +32,14 @@ const Opponent = {
   , Red   : White
 }
 
-class Game extends Logger {
+class Match extends Logger {
 
     constructor() {
         super()
         this.board = new Board()
         this.board.setup()
+        this.cubeOwner = null
+        this.cubeValue = 1
     }
 }
 
@@ -45,65 +51,94 @@ class Board extends Logger {
     }
 
     move(color, i, n) {
+        const move = this.buildMove(color, i, n)
+        move.do()
+        return move
+    }
+
+    buildMove(color, i, n) {
         if (n > 6) {
-            throw new IllegalMoveError('cannot advance more than 6 spaces')
+            throw new InvalidRollError('cannot advance more than 6 spaces')
         }
         if (n < 1) {
-            throw new IllegalMoveError('cannot advance less than 1 space')
+            throw new InvalidRollError('cannot advance less than 1 space')
         }
         if (i == -1) {
-            return this.doComeIn(color, n)
+            return this.buildComeInMove(color, n)
         }
         if (this.hasBar(color)) {
-            throw new IllegalMoveError(sp(color, 'has a piece on the bar'))
+            throw new PieceOnBarError(sp(color, 'has a piece on the bar'))
         }
         const slot = this.slots[i]
         if (slot.length < 1 || slot[0].color != color) {
-            throw new IllegalMoveError(sp(color, 'does not have a piece on space', i + 1))
+            throw new NoPieceOnSlotError(sp(color, 'does not have a piece on slot', i + 1))
         }
         const j = i + n * Direction[color]
         const isBearoff = j < 0 || j > 23
         if (isBearoff) {
             if (!this.mayBearoff(color)) {
-                throw new IllegalMoveError(sp(color, 'may not bare off'))
+                throw new MayNotBearoffError(sp(color, 'may not bare off'))
             }
             // get distance to home
             const homeDistance = Direction[color] == 1 ? 24 - i : i + 1
             // make sure no piece is behind
             if (n > homeDistance && this.hasPieceBehind(color, i)) {
-                throw new IllegalMoveError(sp('cannot bear off with a piece behind'))
+                throw new IllegalBareoffError(sp('cannot bear off with a piece behind'))
             }
-            this.homes[color].push(slot.pop())
+            return {
+                do   : () => this.homes[color].push(slot.pop())
+              , undo : () => slot.push(this.homes[color].pop())
+            }
         } else {
             const dest = this.slots[j]
             if (dest.length > 1 && dest[0].color != color) {
-                throw new IllegalMoveError(sp(color, 'may not occupy space', j + 1))
+                throw new OccupiedSlotError(sp(color, 'may not occupy space', j + 1))
             }
-            if (dest.length == 1 && dest[0].color != color) {
-                this.bars[Opponent[color]].push(dest.pop())
+            const isHit = dest.length == 1 && dest[0].color != color
+            return {
+                do   : () => {
+                    if (isHit) {
+                        this.bars[Opponent[color]].push(dest.pop())
+                    }
+                    dest.push(slot.pop())
+                }
+              , undo : () => {
+                    slot.push(dest.pop())
+                    if (isHit) {
+                        dest.push(this.bars[Opponent[color]].pop())
+                    }
+                }
             }
-            dest.push(slot.pop())
         }
     }
 
-    doComeIn(color, n) {
+    buildComeInMove(color, n) {
         if (!this.hasBar(color)) {
-            throw new IllegalMoveError(sp(color, 'does not have a piece on the bar'))
+            throw new NoPieceOnBarError(sp(color, 'does not have a piece on the bar'))
         }
         const i = Direction[color] == 1 ? n - 1 : 24 - n
         const slot = this.slots[i]
         if (slot.length > 1 && slot[0].color != color) {
-            throw new IllegalMoveError(sp(color, 'cannot come in on space', i + 1))
+            throw new OccupiedSlotError(sp(color, 'cannot come in on space', i + 1))
         }
-        slot.push(this.bars[color].pop())
+        return {
+            do   : () => slot.push(this.bars[color].pop())
+          , undo : () => this.bars[color].push(slot.pop())
+        }
     }
 
-    hasPieceBehind(color, i) {
-        const behinds = Direction[color] == 1 ? intRange(0, i - 1) : intRange(i + 1, 23)
-        return undefined != behinds.find(i =>
-            this.slots[i].length > 0 &&
-            this.slots[i][0].color == color
-        )
+    hasWinner() {
+        return this.getWinner() != null
+    }
+
+    getWinner() {
+        if (this.isAllHome(Red)) {
+            return Red
+        }
+        if (this.isAllHome(White)) {
+            return White
+        }
+        return null
     }
 
     clear() {
@@ -142,18 +177,12 @@ class Board extends Logger {
         return this.homes[color].length == 15
     }
 
-    hasWinner() {
-        return this.getWinner() != null
-    }
-
-    getWinner() {
-        if (this.isAllHome(Red)) {
-            return Red
-        }
-        if (this.isAllHome(White)) {
-            return White
-        }
-        return null
+    hasPieceBehind(color, i) {
+        const behinds = Direction[color] == 1 ? intRange(0, i - 1) : intRange(i + 1, 23)
+        return undefined != behinds.find(i =>
+            this.slots[i].length > 0 &&
+            this.slots[i][0].color == color
+        )
     }
 
     setup() {
@@ -167,16 +196,48 @@ class Board extends Logger {
         this.slots[18] = Piece.make(5, White)
         this.slots[23] = Piece.make(2, Red)
     }
+
+    stateString() {
+        // <White bar count>|<Red bar count>|<slot count>:<Red/White/empty>|...|<White home count>|<Red home count>
+        return [
+            this.bars.White.length
+          , this.bars.Red.length
+        ].concat(this.slots.map(slot =>
+            [slot.length, slot.length > 0 ? slot[0].color : ''].join(':')
+        )).concat([
+            this.homes.White.length
+          , this.homes.Red.length
+        ]).join('|')
+    }
 }
 
 class Piece {
 
     constructor(color) {
         this.color = color
+        this.c = ColorAbbr[color]
     }
 
     static make(n, color) {
         return intRange(0, n - 1).map(i => new Piece(color))
+    }
+}
+
+class Dice {
+
+    static rollOne() {
+        return Math.ceil(Math.random() * 6)
+    }
+
+    static rollTwo() {
+        return [Dice.rollOne(), Dice.rollOne()]
+    }
+
+    static faces(roll) {
+        if (roll[0] == roll[1]) {
+            return roll.concat(roll)
+        }
+        return roll
     }
 }
 
@@ -188,6 +249,21 @@ class GameError extends Error {
     }
 }
 
-class IllegalMoveError extends GameError {}
+class InvalidRollError extends GameError {}
 
-module.exports = {Game, Board, Piece}
+class IllegalMoveError extends GameError {
+
+    constructor(...args) {
+        super(...args)
+        this.isIllegalMoveError = true
+    }
+}
+
+class PieceOnBarError extends IllegalMoveError {}
+class NoPieceOnBarError extends IllegalMoveError {}
+class NoPieceOnSlotError extends IllegalMoveError {}
+class MayNotBearoffError extends IllegalMoveError {}
+class IllegalBareoffError extends IllegalMoveError {}
+class OccupiedSlotError extends IllegalMoveError {}
+
+module.exports = {Match, Board, Piece, Dice}
--- test/test.js
+++ test/test.js
@@ -13,13 +13,13 @@ function getError(cb) {
     }
 }
 
-describe('Game', () => {
+describe('Match', () => {
 
-    const {Game} = Lib
+    const {Match} = Lib
 
     describe('#constructor', () => {
         it('should construct', () => {
-            new Game
+            new Match
         })
     })
 })
@@ -27,6 +27,8 @@ describe('Game', () => {
 describe('Board', () => {
 
     const {Board, Piece} = Lib
+
+    const InitialStateString = '0|0|2:White|0:|0:|0:|0:|5:Red|0:|3:Red|0:|0:|0:|5:White|5:Red|0:|0:|0:|3:White|0:|5:White|0:|0:|0:|0:|2:Red|0|0'
 
     describe('#constructor', () => {
 
@@ -155,19 +157,19 @@ describe('Board', () => {
             board.setup()
         })
 
-        it('should throw for 7 spaces', () => {
+        it('should throw InvalidRollError for 7 spaces', () => {
             const err = getError(() => board.move(White, 0, 7))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('InvalidRollError')
         })
 
-        it('should throw for -1 spaces', () => {
+        it('should throw InvalidRollError for -1 spaces', () => {
             const err = getError(() => board.move(White, 0, -1))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('InvalidRollError')
         })
 
-        it('should throw for comein -1 with no bar', () => {
+        it('should throw NoPieceOnBarError for comein -1 with no bar', () => {
             const err = getError(() => board.move(White, -1, 1))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('NoPieceOnBarError')
         })
 
         it('should comein to n=1 for white with bar', () => {
@@ -182,31 +184,31 @@ describe('Board', () => {
             expect(board.slots[23]).to.have.length(2)
         })
 
-        it('should not comein to n=6 for white with bar', () => {
+        it('should not comein to n=6 for white with bar as OccupiedSlotError', () => {
             board.bars.White.push(board.slots[0].pop())
             const err = getError(() => board.move(White, -1, 6))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('OccupiedSlotError')
         })
 
-        it('should not advance white with bar', () => {
+        it('should not advance white with bar as PieceOnBarError', () => {
             board.bars.White.push(board.slots[0].pop())
             const err = getError(() => board.move(White, 1, 1))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('PieceOnBarError')
         })
 
-        it('should not advance red from empty slot', () => {
+        it('should not advance red from empty slot as NoPieceOnSlotError', () => {
             const err = getError(() => board.move(Red, 2, 1))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('NoPieceOnSlotError')
         })
 
-        it('should not advance red from slot owned by white', () => {
+        it('should not advance red from slot owned by white as NoPieceOnSlotError', () => {
             const err = getError(() => board.move(Red, 0, 1))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('NoPieceOnSlotError')
         })
 
-        it('should not bear off white with piece outside', () => {
+        it('should not bear off white with piece outside as MayNotBearoffError', () => {
             const err = getError(() => board.move(White, 18, 6))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('MayNotBearoffError')
         })
 
         it('should bear off white from 6 point with all other pieces on 5 point', () => {
@@ -239,22 +241,22 @@ describe('Board', () => {
             expect(board.homes.Red).to.have.length(1)
         })
 
-        it('should not bear off white with n=6 from 5 point with piece behind', () => {
+        it('should not bear off white with n=6 from 5 point with piece behind as IllegalBareoffError', () => {
             board.slots[19] = board.slots[0].splice(0).concat(
                 board.slots[11].splice(0),
                 board.slots[16].splice(0)
             )
             const err = getError(() => board.move(White, 19, 6))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('IllegalBareoffError')
         })
 
-        it('should not bear off red with n=6 from 5 point with piece behind', () => {
+        it('should not bear off red with n=6 from 5 point with piece behind as IllegalBareoffError', () => {
             board.slots[4] = board.slots[23].splice(0).concat(
                 board.slots[12].splice(0),
                 board.slots[7].splice(0)
             )
             const err = getError(() => board.move(Red, 4, 6))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('IllegalBareoffError')
         })
 
         it('should advance white from 0 to 1', () => {
@@ -263,9 +265,9 @@ describe('Board', () => {
             expect(board.slots[1]).to.have.length(1)
         })
 
-        it('should not advance white from 0 to 5', () => {
+        it('should not advance white from 0 to 5 as OccupiedSlotError', () => {
             const err = getError(() => board.move(White, 0, 5))
-            expect(err.name).to.equal('IllegalMoveError')
+            expect(err.name).to.equal('OccupiedSlotError')
         })
 
         it('should move white to bar when red hits on 1', () => {
@@ -274,6 +276,117 @@ describe('Board', () => {
             expect(board.slots[1]).to.have.length(1)
             expect(board.slots[1][0].color).to.equal(Red)
             expect(board.bars.White).to.have.length(1)
+        })
+
+        it('should return expected state string after white moves 2 pips for one runner', () => {
+            board.move(White, 0, 2)
+            const exp = '0|0|1:White|0:|1:White|0:|0:|5:Red|0:|3:Red|0:|0:|0:|5:White|5:Red|0:|0:|0:|3:White|0:|5:White|0:|0:|0:|0:|2:Red|0|0'
+            const result = board.stateString()
+            expect(result).to.equal(exp)
+        })
+
+        it('should return initial state string after undoing white i:0,n:2', () => {
+            const move = board.move(White, 0, 2)
+            move.undo()
+            const result = board.stateString()
+            const exp = InitialStateString
+            expect(result).to.equal(exp)
+        })
+
+        it('should return expected state string after white i:0,n:2, then undoing red i:5,n:3 hit', () => {
+            board.move(White, 0, 2)
+            const move = board.move(Red, 5, 3)
+            move.undo()
+            const result = board.stateString()
+            const exp = '0|0|1:White|0:|1:White|0:|0:|5:Red|0:|3:Red|0:|0:|0:|5:White|5:Red|0:|0:|0:|3:White|0:|5:White|0:|0:|0:|0:|2:Red|0|0'
+            expect(result).to.equal(exp)
+        })
+
+        it('should undo bareoff on sparse board white i:22,n:3', () => {
+            board.clear()
+            board.slots[22] = Piece.make(2, White)
+            const move = board.move(White, 22, 3)
+            expect(board.slots[22].length).to.equal(1)
+            expect(board.homes.White).to.have.length(1)
+            move.undo()
+            expect(board.slots[22].length).to.equal(2)
+            expect(board.homes.White).to.have.length(0)
+        })
+
+        it('should undo comein on sparse board white i:-1,n:2', () => {
+            board.clear()
+            board.bars.White.push(new Piece(White))
+            const move = board.move(White, -1, 2)
+            expect(board.bars.White).to.have.length(0)
+            expect(board.slots[1]).to.have.length(1)
+            move.undo()
+            expect(board.bars.White).to.have.length(1)
+            expect(board.slots[1]).to.have.length(0)
+        })
+    })
+
+    describe('#stateString', () => {
+
+        it('should return all zeros and no slot colors for blank board', () => {
+            const board = new Board
+            const result = board.stateString()
+            const exp = '0|0|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0:|0|0'
+            expect(result).to.equal(exp)
+        })
+
+        it('should return expected value for setup board', () => {
+            const board = new Board
+            board.setup()
+            const result = board.stateString()
+            const exp = InitialStateString
+            expect(result).to.equal(exp)
+        })
+    })
+
+})
+describe('Dice', () => {
+
+    const {Dice} = Lib
+
+    describe('#rollOne', () => {
+
+        it('should return number between 1 and 6 for 100 rolls', () => {
+            for (var i = 0; i < 100; i ++) {
+                var result = Dice.rollOne()
+                expect(result).to.be.greaterThan(0)
+                expect(result).to.be.lessThan(7)
+            }
+        })
+    })
+
+    describe('#rollTwo', () => {
+
+        it('should have length 2', () => {
+            const result = Dice.rollTwo()
+            expect(result).to.have.length(2)
+        })
+
+        it('should return numbers between 1 and 6 for 100 rolls', () => {
+            for (var i = 0; i < 100; i ++) {
+                var result = Dice.rollTwo()
+                expect(result[0]).to.be.greaterThan(0)
+                expect(result[0]).to.be.lessThan(7)
+                expect(result[1]).to.be.greaterThan(0)
+                expect(result[1]).to.be.lessThan(7)
+            }
+        })
+    })
+
+    describe('#faces', () => {
+
+        it('should return [1, 2] for [1, 2]', () => {
+            const result = Dice.faces([1, 2])
+            expect(JSON.stringify(result)).to.equal(JSON.stringify([1, 2]))
+        })
+
+        it('should return [5, 5, 5, 5] for [5, 5]', () => {
+            const result = Dice.faces([5, 5])
+            expect(JSON.stringify(result)).to.equal(JSON.stringify([5, 5, 5, 5]))
         })
     })
 })
