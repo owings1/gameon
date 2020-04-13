dd9996f042199231c50e097906bd5db64573bd22
Doug Owings <doug@dougowings.net>
2020-04-13 11:12:21 -0700

    record methods

--- src/lib/coordinator.js
+++ src/lib/coordinator.js
@@ -1,13 +1,30 @@
 const Core = require('../lib/core')
 const Logger = require('../lib/logger')
 
+const fse   = require('fs-extra')
+const merge = require('merge')
+const path  = require('path')
+
 const {White, Red, Opponent} = Core
+
+const DefaultOpts = {
+    isRecord  : false
+  , recordDir : null
+}
 
 class Coordinator {
 
-    constructor() {
+    constructor(opts) {
         this.holds = []
         this.logger = new Logger
+        this.opts = merge(DefaultOpts, opts)
+        if (this.opts.isRecord) {
+            try {
+                this.opts.recordDir = path.resolve(this.opts.recordDir)
+            } catch (err) {
+                throw new Error('invalid recordDir', err)
+            }
+        }
     }
 
     async runMatch(match, white, red) {
@@ -15,14 +32,28 @@ class Coordinator {
             White : white
           , Red   : red
         }
+        if (this.opts.isRecord) {
+            var matchDir = path.resolve(this.opts.recordDir, ['match', match.uuid].join('_'))
+            await fse.ensureDir(matchDir)
+        }
         await this.emitAll(players, 'matchStart', match)
         do {
             await this.emitAll(players, 'beforeNextGame', match, players)
             await this.runGame(players, match.nextGame(), match)
+            if (this.opts.isRecord) {
+                this.logger.info('foooo')
+                var gameFile = path.resolve(matchDir, ['game', match.games.length].join('_') + '.json')
+                await this.recordGame(match.thisGame, gameFile, players)
+            }
             match.updateScore()
         } while (!match.hasWinner())
 
         await this.emitAll(players, 'matchEnd', match)
+        if (this.opts.isRecord) {
+            this.logger.info('barrrr')
+            const matchFile = path.resolve(matchDir, 'match.json')
+            await this.recordMatch(match, matchFile, players)
+        }
     }
 
     async runGame(players, game, match) {
@@ -69,6 +100,28 @@ class Coordinator {
         } while (!game.checkFinished())
 
         await this.emitAll(players, 'gameEnd', game, match)
+    }
+
+    async recordMatch(match, file, players) {
+        this.logger.info('Recording match')
+        const meta = merge(match.meta(), {
+            players : {
+                White : players.White.meta()
+              , Red   : players.Red.meta()
+            }
+        })
+        await fse.writeJson(file, meta, {spaces: 2})
+    }
+
+    async recordGame(game, file, players) {
+        this.logger.info('Recording game')
+        const meta = merge(game.meta(), {
+            players : {
+                White : players.White.meta()
+              , Red   : players.Red.meta()
+            }
+        })
+        await fse.writeJson(file, meta, {spaces: 2})
     }
 
     async emitAll(emitters, ...args) {
