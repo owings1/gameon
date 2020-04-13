2f673269926a38855f07de838086d3da23b71206
Doug Owings <doug@dougowings.net>
2020-04-13 11:11:08 -0700

    destroy and meta methods

--- src/lib/player.js
+++ src/lib/player.js
@@ -8,11 +8,16 @@ class Player extends EventEmitter {
 
     constructor(color) {
         super()
+        this.name = this.constructor.name
         this.color = color
         this.on('matchStart', match => this.thisMatch = match)
         this.on('gameStart', (game, match, players) => {
             this.thisGame = game
         })
+    }
+
+    destroy() {
+        this.removeAllListeners()
     }
 
     async rollTurn(turn, game, match) {
@@ -29,6 +34,10 @@ class Player extends EventEmitter {
 
     async playRoll(turn, game, match) {
         throw new Error('NotImplemented')
+    }
+
+    meta() {
+        return {name: this.name, color: this.color}
     }
 }
 
