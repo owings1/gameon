c7f34b6f8e49ce4c2339c18bf5ecc3f5eb45dccd
Doug Owings <doug@dougowings.net>
2021-05-31 21:30:37 -0700

    running robot optimization

--- src/robot/robots/running.js
+++ src/robot/robots/running.js
@@ -24,28 +24,45 @@
  */
 const Base = require('../player').ConfidenceRobot
 
-const {sumArray}  = require('../../lib/util')
+const {intRange} = require('../../lib/util')
+
+function quadrantMultiplier(point) {
+    const quadrant = Math.ceil(point / 6)
+    if (quadrant == 4) {
+        return 8
+    }
+    return (quadrant - 1) / 2
+}
+
+const QuadrantMultipliers = {}
+
+intRange(1, 24).forEach(point =>
+    QuadrantMultipliers[point] = quadrantMultiplier(point)
+)
 
 class RunningRobot extends Base {
 
     async getRankings(turn, game, match) {
         const scores = {}
-        //const bkBefore = turn.board.analyzer.countPiecesInPointRange(turn.color, 19, 24)
-        turn.allowedEndStates.forEach(endState => {
-            const {analyzer} = turn.fetchBoard(endState)
-            scores[endState] = sumArray(analyzer.pointsOccupied(turn.color).map(point =>
-                point * analyzer.piecesOnPoint(turn.color, point) * this.quadrantMultiplier(point)
-            ))
-        })
+        const len = turn.allowedEndStates.length
+        for (var i = 0; i < len; i++) {
+            var endState = turn.allowedEndStates[i]
+            scores[endState] = this._rankEndState(turn, endState)
+        }
+        // Inverse ranking
         return this.spreadRanking(scores, true)
     }
 
-    quadrantMultiplier(point) {
-        const quadrant = Math.ceil(point / 6)
-        if (quadrant == 4) {
-            return 8
+    _rankEndState(turn, endState) {
+        const {analyzer} = turn.fetchBoard(endState)
+        const points = analyzer.pointsOccupied(turn.color)
+        const len = points.length
+        var score = 0
+        for (var i = 0; i < len; i++) {
+            var point = points[i]
+            score += point * analyzer.piecesOnPoint(turn.color, point) * QuadrantMultipliers[point]
         }
-        return (quadrant - 1) / 2
+        return score
     }
 }
 
