278a29bc51a6cb30a8a2a437e8dd341064b5944d
Doug Owings <doug@dougowings.net>
2021-06-06 16:01:47 -0700

    add copyright headers to new files

--- src/lib/analyzer.js
+++ src/lib/analyzer.js
@@ -1,8 +1,31 @@
+/**
+ * gameon - BoardAnalyzer class
+ *
+ * Copyright (C) 2020-2021 Doug Owings
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 const Core = require('./core')
 
 const {
-    Colors
-  , Direction
+    Direction
   , Opponent
   , OriginPoints
   , OutsideOrigins
@@ -10,7 +33,7 @@ const {
   , Profiler
 } = Core
 
-const {Red, White} = Colors
+const {Red, White} = Core.Colors
 const {IllegalStateError} = Core.Errors
 
 const CacheKeys = {}
@@ -47,6 +70,7 @@ function populateCacheKeys(keys) {
 }
 
 populateCacheKeys(CacheKeys)
+
 // NB: Caching methods return a reference for performance. Callers must make a copy
 ///    if they will modify the result
 class BoardAnalyzer {
