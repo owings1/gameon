1591d44beaf308e8912cb6ca0872180280341778
Doug Owings <doug@dougowings.net>
2021-06-12 03:58:29 -0700

    cleanup

---
+++ extras/other.js
@@ -0,0 +1,19 @@
+  /*
+, WayOffbeat : {
+      styles : {
+          'text.background'             : 'cyan'
+        , 'text.color'                  : 'blue'
+        , 'board.background'            : 'green'
+        , 'board.piece.white.color'     : '#0080ff bold'
+        , 'board.piece.red.color'       : 'orange bold'
+        , 'board.border.background'     : 'magenta'
+        , 'board.border.color'          : 'black'
+        , 'board.pointLabel.background' : 'red'
+        , 'board.pointLabel.color'      : 'white'
+        , 'text.piece.red.color'        : '#ff0088'
+        , 'text.dice.color'             : 'purple'
+        , 'cube.active.color'           : 'green'
+        , 'cube.inactive.color'         : 'grey'
+      }
+  }
+  */
