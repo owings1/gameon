c0a6ba4581510f7205805e29156a150be81d815b
Doug Owings <doug@dougowings.net>
2021-08-27 04:12:08 -0700

    add lab translations

--- src/term/lab.js
+++ src/term/lab.js
@@ -153,10 +153,11 @@ class LabHelper {
 
     async interactive() {
         this.draw(true)
+        const {__} = this
         while (true) {
             const answers = await this.prompt({
                 name    : 'input',
-                message : 'Input',
+                message : __('lab.question.command'),
                 type    : 'input',
             })
             const {input} = answers
@@ -170,7 +171,7 @@ class LabHelper {
                 break
             }
             if (inputLc === '?') {
-                this.println(LabHelper.commandHelp())
+                this.println(this.commandHelp())
                 continue
             }
             await this.runCommand(input)
@@ -181,6 +182,7 @@ class LabHelper {
         if (isPrintFirst) {
             this.draw(true)
         }
+        const {__} = this
         const inputLc = input.toLowerCase()
         const [cmd, ...params] = input.split(' ')
         const cmdLc = cmd.toLowerCase()
@@ -207,14 +209,16 @@ class LabHelper {
 
             case 'f':
                 this.persp = Opponent[this.persp]
-                this.logs.push(sp('Perspective', this.ccolor(this.persp)))
+                this.logs.push(__('play.log.setPerspective{color}', {
+                    color: this.ccolor(this.persp, __(`play.color.${this.persp}`)),
+                }))
                 this.drawer.persp = this.persp
                 await this.draw(true)
                 break
 
             case 'F':
                 this.board.setStateString(this.board.inverted().state28())
-                this.logs.push('Invert board')
+                this.logs.push(__('play.log.invertBoard'))
                 await this.draw(true)
                 break
 
@@ -240,13 +244,17 @@ class LabHelper {
 
             case 'x':
                 this.opts.breadthTrees = !this.opts.breadthTrees
-                const treeStyle = this.opts.breadthTrees ? 'breadth' : 'depth'
-                this.println(`Using ${treeStyle} trees`)
+                const treeStyle = this.opts.breadthTrees
+                    ? __('core.treeStyle.breadth')
+                    : __('core.treeStyle.depth')
+                this.println(
+                    __('alerts.usingTreeStyleTrees{treeStyle}', {treeStyle})
+                )
                 break
 
             default:
                 this.logger.warn(__('alerts.invalidCommand{input}', {input}))
-                this.println(LabHelper.commandHelp())
+                this.println(this.commandHelp())
                 break
         }
     }
@@ -256,7 +264,7 @@ class LabHelper {
         const answers = await this.prompt({
             name     : 'state',
             type     : 'input',
-            message  : __('menu.question.stateString'),
+            message  : __('lab.question.stateString'),
             when     : () => !param,
             default  : () => board.stateString(),
             validate : value => this.validateStateString(value),
@@ -291,8 +299,7 @@ class LabHelper {
         this.fetchLastRecords = null
         this.stateHistory.push(board.state28())
         board.setStateString(newState)
-        // TODO: report __
-        this.logs.push('Set state')
+        this.logs.push(__('play.log.setState'))
     }
 
     async undoCommand() {
@@ -303,17 +310,16 @@ class LabHelper {
         }
         this.board.setStateString(this.stateHistory.pop())
         this.fetchLastRecords = null
-        // TODO: report __
-        this.logs.push('Undo')
+        this.logs.push(__('play.log.undo'))
     }
 
     async diceCommand(isRobot, param) {
-        const {board} = this
+        const {board, __} = this
         const parseInput = value => {
             return value.split(',').map(it => parseInt(it.trim()))
         }
         const answers = await this.prompt({
-            message  : 'Dice',
+            message  : __('lab.question.dice'),
             type     : 'input',
             name     : 'dice',
             validate : value => this.validateDice(parseInput(value)),
@@ -325,10 +331,9 @@ class LabHelper {
         const turn = new Turn(board, this.persp, this.opts).setRoll(dice)
         if (turn.isCantMove) {
             this.println(__('alerts.noMovesForColorWithRoll{color,roll}', {
-                color : __(this.persp),
+                color : __(`play.color.${this.persp}`),
                 roll  : dice.join(','),
             }))
-            this.println(`No moves for ${this.persp} with ${dice.join()}`)
             return
         }
         if (isRobot) {
@@ -353,7 +358,7 @@ class LabHelper {
         const hr = nchars(39, Chars.table.dash)
         log(hr)
         log(info)
-        log('  Move Series:')
+        log('  ' + __('lab.title.moveSeries') + ':')
         series.forEach((moves, i) =>
             log('   ', (i+1) + ':', moves.map(move => this.moveDesc(move)))
         )
@@ -401,6 +406,7 @@ class LabHelper {
 
     showRobotTurnRankList(rankList, delegateWidth) {
         const {theme, __} = this
+        const {nf} = this.intl
         const chlk = theme.table
         const indent = 2
         const b = new StringBuilder
@@ -457,27 +463,38 @@ class LabHelper {
                 info.moves.map(moveDesc).join(chlk.row.dim(', ')),
                 chlk.row.dim(']'),
             )
+            const footTitles = {
+                score: __('lab.title.score'),
+                rank : __('lab.title.rank'),
+                state: __('lab.title.state'),
+            }
+            const footTitleWidth = Math.max(...Object.values(footTitles).map(stringWidth))
+            Object.keys(footTitles).forEach(key => {
+                footTitles[key] = footTitles[key].padEnd(footTitleWidth, ' ') + ' : '
+            })
             const bscore = new StringBuilder(
-                chlk.foot('Score : '),
+                chlk.foot(footTitles.score),
                 chlk.foot.cyan.bold(info.finalScore.toFixed(4)),
             )
             if (lastScore > 0) {
-                const decreasePct = Math.round(100 * (lastScore - info.finalScore) / lastScore)
-                if (decreasePct) {
+                const decrease = (lastScore - info.finalScore) / lastScore
+                if (decrease >= 0.01) {
                     bscore.add(
                         chlk.foot(' '),
-                        chlk.foot.red.bold(Chars.arrow.down + decreasePct + '%'),
+                        chlk.foot.red.bold(
+                            Chars.arrow.down + nf(decrease, {style: 'percent'})
+                        ),
                     )
                 }
             }
             const footerLines = [
                 cat(
-                    chlk.foot('Rank  : '),
+                    chlk.foot(footTitles.rank),
                     chlk.title(info.rank.toString()),
                 ),
                 bscore.toString(),
                 cat(
-                    chlk.foot('State : '),
+                    chlk.foot(footTitles.state),
                     chlk.foot.dim(info.endState),
                 ),
             ]
@@ -608,7 +625,11 @@ class LabHelper {
         const coordinator = new Coordinator
         const matches = []
         try {
-            logger.info('Running', numMatches, 'matches', this.persp, 'goes first')
+            const params = {
+                color: __(`play.color.${this.persp}`),
+                count: numMatches,
+            }
+            logger.info(__('alerts.runningCountMatchesColorGoesFirst{count,color}', params))
             for (let i = 0; i < numMatches; ++i) {
                 const match = new Match(1, matchOpts)
                 await coordinator.runMatch(match, players)
@@ -623,8 +644,12 @@ class LabHelper {
             scores.White += meta.scores.White
         })
         const diff = scores[this.persp] - scores[Opponent[this.persp]]
-        this.println('scores:', stringify(scores))
-        this.println('diff:', this.chalkDiff(diff))
+        const scoresTran = {}
+        for (const color in scores) {
+            scoresTran[__(`play.color.${color}`)] = scores[color]
+        }
+        this.println(__('lab.attr.scores') + ':', stringify(scoresTran))
+        this.println(__('lab.attr.diff') + ':', this.chalkDiff(diff))
     }
 
     async placeCommand() {
@@ -643,8 +668,7 @@ class LabHelper {
         }
         this.fetchLastRecords = null
         this.stateHistory.push(board.state28())
-        const b = new StringBuilder
-        b.add('Place')
+        const params = {}
         let piece
         if (answers.from === 'b') {
             let color
@@ -654,8 +678,7 @@ class LabHelper {
                 color = analyzer.hasBar(White) ? White : Red
             }
             piece = board.popBar(color)
-            b.sp(this.ccolor(piece.color), 'bar')
-
+            params.origin = __('play.board.bar')
         } else if (answers.from === 'h') {
             let color
             if (analyzer.piecesHome(White) && analyzer.piecesHome(Red)) {
@@ -664,27 +687,27 @@ class LabHelper {
                 color = analyzer.piecesHome(White) ? White : Red
             }
             piece = board.popHome(color)
-            b.sp(piece.color, 'home')
+            params.origin = __('play.board.home')
         } else {
             const fromPoint = parseInt(answers.from)
             const fromOrigin = PointOrigins[this.persp][fromPoint]
             piece = board.popOrigin(fromOrigin)
-            b.sp(piece.color, fromPoint)
+            params.origin = String(fromPoint)
         }
-        b.add(':')
+        params.color = this.ccolor(piece.color, __(`play.color.${piece.color}`))
         if (answers.dest === 'b') {
             board.pushBar(piece.color, piece)
-            b.add('bar')
+            params.dest = __('play.board.bar')
         } else if (answers.dest === 'h') {
             board.pushHome(piece.color, piece)
-            b.add('home')
+            params.dest = __('play.board.home')
         } else {
             const destPoint = parseInt(answers.dest)
             const destOrigin = PointOrigins[this.persp][destPoint]
             board.pushOrigin(destOrigin, piece)
-            b.add(destPoint)
+            params.dest = String(destPoint)
         }
-        this.logs.push(b.join(' '))
+        this.logs.push(__('play.log.place{color,origin,dest}', params))
     }
 
     boardInfo() {
@@ -700,18 +723,19 @@ class LabHelper {
     }
 
     moveParts({origin, face}) {
+        const {__} = this
         const parts = []
         let startPoint
         if (origin === -1) {
             startPoint = 25
-            parts.push('bar')
+            parts.push(__('play.board.bar'))
         } else {
             startPoint = OriginPoints[this.persp][origin]
             parts.push(startPoint)
         }
         let destPoint = startPoint - face
         if (destPoint < 1) {
-            destPoint = 'home'
+            destPoint = __('play.board.home')
         }
         parts.push(destPoint)
         return parts
@@ -731,7 +755,7 @@ class LabHelper {
 
     getPlaceQuestions() {
 
-        const {board} = this
+        const {board, __} = this
         const {analyzer} = board
 
         return [
@@ -781,38 +805,43 @@ class LabHelper {
     }
 
     validatePlaceFrom(value) {
+        const {__} = this
         const {analyzer} = this.board
         if (value.toLowerCase() === 'q') {
             return true
         }
         if (value === 'b') {
             if (!analyzer.hasBar(White) && !analyzer.hasBar(Red)) {
-                return 'No pieces on bar'
+                return __('alerts.noPieceOnBar')
             }
             return true
         }
         if (value === 'h') {
             if (!analyzer.piecesHome(White) && !analyzer.piecesHome(Red)) {
-                return 'No pieces on hom'
+                return __('alerts.noPieceOnHome')
             }
             return true
         }
         const point = parseInt(value)
         if (isNaN(point) || point < 1 || point > 24) {
-            return 'Invalid point'
+            return __('alerts.invalidPoint')
         }
         const origin = PointOrigins[this.persp][point]
-        return !!analyzer.originOccupier(origin) || sp('No piece on point', point)
+        return (
+            Boolean(analyzer.originOccupier(origin)) ||
+            __('alerts.noPieceOnPoint{point}', {point})
+        )
     }
 
     validatePlaceTo(value, answers) {
+        const {__} = this
         const {analyzer} = this.board
         if (value === 'b' || value === 'h' || value.toLowerCase() === 'q') {
             return true
         }
         const point = parseInt(value)
         if (isNaN(point) || point < 1 || point > 24) {
-            return 'Invalid point'
+            return __('alerts.invalidPoint')
         }
         const origin = PointOrigins[this.persp][point]
         const occupier = analyzer.originOccupier(origin)
@@ -830,7 +859,14 @@ class LabHelper {
                 color = analyzer.originOccupier(fromOrigin)
             }
         }
-        return !occupier || occupier === color || sp(point, 'occupied by', occupier)
+        return (
+            !occupier ||
+            occupier === color ||
+            __('alerts.pointOccupiedByColor{point,color}', {
+                color: __(`play.color.${occupier}`),
+                point,
+            })
+        )
     }
 
     validateStateString(value) {
@@ -870,7 +906,10 @@ class LabHelper {
         return this.prompter
     }
 
-    ccolor(color) {
+    ccolor(color, str = null) {
+        if (str === null) {
+            str = color
+        }
         const chlk = this.theme.board
         return chlk.piece[color.toLowerCase()](color)
     }
@@ -897,26 +936,27 @@ class LabHelper {
     }
 
     async writeLastResult() {
+        const {logger, __} = this
         if (!this.opts.recordDir) {
-            this.logger.warn('No recordDir set')
+            logger.warn(__('alerts.noRecordDirSet'))
             return false
         }
         this.lastOutDir = null
         if (!this.fetchLastRecords) {
-            this.logger.info('No result to save')
+            logger.info(__('alerts.noResultToSave'))
             return false
         }
         const subDir = 'labs'
         const prefix = 'lab-' + fileDateString()
         const outDir = path.resolve(this.opts.recordDir, subDir, prefix)
-        this.logger.info('Saving to', homeTilde(outDir))
+        logger.info(__('alerts.savingTo{path}', {path: homeTilde(outDir)}))
         await fse.ensureDir(outDir)
         const records = await this.fetchLastRecords()
         this.fetchLastRecords = null
         for (const [basename, data] of Object.entries(records)) {
             const file = path.resolve(outDir, path.basename(basename))
             fs.writeFileSync(file, data)
-            this.logger.info('   ', basename)
+            logger.info('   ', basename)
         }
         const lab = {
             date    : new Date,
@@ -926,7 +966,7 @@ class LabHelper {
         const basename = 'lab.json'
         const labFile = path.resolve(outDir, basename)
         await fse.writeJson(labFile, lab, {spaces: 2})
-        this.logger.info('   ', basename)
+        logger.info('   ', basename)
         this.lastOutDir = outDir
         return true
     }
@@ -1003,23 +1043,24 @@ class LabHelper {
         this.output.write((args.length ? args.join(' ') : '') + '\n')
     }
 
-    static commandHelp() {
+    commandHelp() {
+        const {__} = this
         const helps = {
-            'i' : 'board info',
-            's' : 'set state of board',
-            'd' : 'show moves for dice',
-            'D' : 'show robot info for dice',
-            'f' : 'flip perspective',
-            'F' : 'flip (invert) board',
-            'p' : 'place piece',
-            'r' : 'rollout',
-            'u' : 'undo move',
-            'w' : 'write last results',
-            'x' : 'toggle tree mode',
-            '?' : 'command help',
+            'i' : __('lab.commandHelp.boardInfo'),
+            's' : __('lab.commandHelp.setBoardState'),
+            'd' : __('lab.commandHelp.showMovesForDice'),
+            'D' : __('lab.commandHelp.showRobotInfoForDice'),
+            'f' : __('lab.commandHelp.flipPerspective'),
+            'F' : __('lab.commandHelp.invertBoard'),
+            'p' : __('lab.commandHelp.placePiece'),
+            'r' : __('lab.commandHelp.rollout'),
+            'u' : __('lab.commandHelp.undoMove'),
+            'w' : __('lab.commandHelp.writeLastResults'),
+            'x' : __('lab.commandHelp.toggleTreeMode'),
+            '?' : __('lab.commandHelp.commandHelp'),
         }
         const b = new StringBuilder(
-            'Commands:',
+            __('lab.title.commands') + ':',
             '---------',
         )
         Object.entries(helps).forEach(it =>
